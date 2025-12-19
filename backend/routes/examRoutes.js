6d1fceb622c0bbe38f6366f31d05731cda41a5af
Mahmoud-Saher <mahmodsaher07@gmail.com>
2025-12-20 01:29:44 +0200

    Update examRoutes.js

--- backend/routes/examRoutes.js
+++ backend/routes/examRoutes.js
@@ -1,28 +1,20 @@
-// backend/routes/examRoutes.js
 
 const express = require('express');
 const router = express.Router();
 const examController = require('../controllers/examController');
 
-// 1. إنشاء امتحان
 router.post('/', examController.createExam);
 
-// 2. جلب امتحانات المعلم (Query Param)
 router.get('/', examController.getExamsByInstructor);
 
-// 3. جلب كل الامتحانات
 router.get('/all', examController.getAllExams);
 
-// 4. البحث بالكود
 router.get('/access/:code', examController.getExamByCode);
 
-// 5. حذف امتحان
 router.delete('/:id', examController.deleteExam);
 
-// 6. جلب نتائج امتحان (لازم قبل الـ /:id العادية عشان الترتيب)
 router.get('/:id/results', examController.getExamResults);
 
-// 7. جلب تفاصيل امتحان واحد بالـ ID
 router.get('/:id', examController.getExamById);
 
 module.exports = router;
