2bdb3110f27203bffb27c5fd21e638e6e0edbd84
Mahmoud-Saher <mahmodsaher07@gmail.com>
2025-12-20 01:32:35 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,14 @@
-// src/main.jsx
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // ملف الـ CSS العام
+import './index.css'; 
 
-// 1. استيراد الراوتر
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    {/* 2. إحاطة التطبيق بالراوتر */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
