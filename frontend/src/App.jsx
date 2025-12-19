3f6dc2d0a55f1f8825f69d60389d0ef14ad79ac7
Mahmoud-Saher <mahmodsaher07@gmail.com>
2025-12-20 01:33:51 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,22 @@
-// src/App.jsx
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-// استيراد الصفحات
 import AuthPage from './pages/AuthPage';
 import StudentDashboard from './pages/StudentDashboard';
 import InstructorDashboard from './pages/InstructorDashboard';
 import ExamPage from './pages/ExamPage';
 import ResultsPage from './pages/ResultsPage';
 
-// 1. استيراد التخطيط الجديد
 import MainLayout from './components/layout/MainLayout';
 
 function App() {
   return (
     <Routes>
-      {/* 2. الصفحات التي لا تحتوي على هيدر (مثل اللوجن) */}
       <Route path="/login" element={<AuthPage />} />
       <Route path="/" element={<AuthPage />} />
 
-      {/* 3. الصفحات التي تحتوي على الهيدر (داخل التخطيط) */}
+     
       <Route element={<MainLayout />}>
         <Route path="/student" element={<StudentDashboard />} />
         <Route path="/instructor" element={<InstructorDashboard />} />
