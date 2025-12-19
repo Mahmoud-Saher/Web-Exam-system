9ed5d48e78df38d60f513acd1bede359150c7ed8
Mahmoud-Saher <mahmodsaher07@gmail.com>
2025-12-20 01:30:04 +0200

    Remove comments from questionRoutes.js

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -1,17 +1,12 @@
-// backend/routes/questionRoutes.js
 
 const express = require('express');
 const router = express.Router();
 const questionController = require('../controllers/questionController');
 
-// POST: لإضافة سؤال
 router.post('/', questionController.createQuestion);
 
-// POST: لإضافة مجموعة أسئلة من ملف Excel
 router.post('/bulk', questionController.createBulkQuestions);
 
-// GET: لجلب الأسئلة (هتكون كده: /api/questions?userId=1)
 router.get('/', questionController.getQuestionsByInstructor);
-// DELETE: لحذف سؤال
 router.delete('/:id', questionController.deleteQuestion);
 module.exports = router;
