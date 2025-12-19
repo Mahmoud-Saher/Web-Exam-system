4de57aa7190ac7f086730b91cd57424f13b7801e
Mahmoud-Saher <mahmodsaher07@gmail.com>
2025-12-20 01:29:08 +0200

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,13 @@
-// backend/routes/authRoutes.js
+
 
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// تعريف الروابط
-// POST: http://localhost:3001/api/auth/signup
+
 router.post('/signup', authController.register);
 
-// POST: http://localhost:3001/api/auth/login
+
 router.post('/login', authController.login);
 
 module.exports = router;
