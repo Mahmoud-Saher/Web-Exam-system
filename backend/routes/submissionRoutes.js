a074c68716764f26492e62be35bd4ca7b044070b
Mahmoud-Saher <mahmodsaher07@gmail.com>
2025-12-20 01:30:23 +0200

    Update submissionRoutes.js

--- backend/routes/submissionRoutes.js
+++ backend/routes/submissionRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const submissionController = require('../controllers/submissionController');
 
-// POST: إرسال الحل
 router.post('/', submissionController.submitExam);
-// GET: جلب سجل الامتحانات للطالب
 router.get('/history', submissionController.getStudentHistory); 
 module.exports = router;
