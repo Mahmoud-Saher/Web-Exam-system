94f5f1b1b2a76ac30015e988cc561ff90292d78b
Mahmoud-Saher <mahmodsaher07@gmail.com>
2025-12-20 01:31:27 +0200

    Refactor server.js for clarity and consistency

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const { connectDB, sequelize } = require('./config/database');
 
-// 1. استيراد الموديلز
 const User = require('./models/User');
 const Question = require('./models/Question');
 const Exam = require('./models/Exam');
-const Submission = require('./models/Submission'); // الموديل
+const Submission = require('./models/Submission'); 
 
-const submissionRoutes = require('./routes/submissionRoutes'); // الراوت
+const submissionRoutes = require('./routes/submissionRoutes'); 
 const authRoutes = require('./routes/authRoutes');
 const questionRoutes = require('./routes/questionRoutes');
 const examRoutes = require('./routes/examRoutes');
@@ -16,47 +15,38 @@ const examRoutes = require('./routes/examRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
-// تسجيل الراوتس
 app.use('/api/exams', examRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/questions', questionRoutes);
 app.use('/api/submissions', submissionRoutes);
 
-// 2. تعريف العلاقات (مين يملك مين؟)
-// الطالب (User) له نتائج (Submissions)
 User.hasMany(Submission);
 Submission.belongsTo(User);
 
-// الامتحان (Exam) له نتائج
 Exam.hasMany(Submission);
 Submission.belongsTo(Exam);
 
-// المعلم يملك أسئلة وامتحانات
 User.hasMany(Question);
 Question.belongsTo(User);
 
 User.hasMany(Exam);
 Exam.belongsTo(User);
 
-// الامتحان والأسئلة (علاقة متداخلة Many-to-Many)
-// يعني الامتحان فيه أسئلة كتير، والسؤال ممكن يتحط في كذا امتحان
 Exam.belongsToMany(Question, { through: 'ExamQuestions' });
 Question.belongsToMany(Exam, { through: 'ExamQuestions' });
 
 
-// 3. تشغيل السيرفر وبناء الجداول
 const PORT = 3001;
 
 const startServer = async () => {
   try {
     await connectDB();
     
-    // الأمر السحري: ده اللي هيروح الداتابيز وينشئ الجداول
     await sequelize.sync({ alter: true });
-    console.log('✅ All Tables created successfully!');
+    console.log('All Tables created successfully!');
 
     app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+      console.log(` Server running on http://localhost:${PORT}`);
     });
 
   } catch (error) {
