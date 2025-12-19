57a12d8327ad02ec8b606c29d0482650124aad47
Mahmoud-Saher <mahmodsaher07@gmail.com>
2025-12-20 01:23:35 +0200

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,19 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-// إعداد الاتصال بقاعدة البيانات
-// ('اسم_القاعدة', 'اسم_المستخدم', 'الباسورد')
 const sequelize = new Sequelize('exam_system_db', 'root', '12345', {
   host: 'localhost',
   dialect: 'mysql',
-  logging: false // عشان منزحمش التيرمينال
+  logging: false 
 });
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log('✅ Database connected...');
+    console.log(' Database connected...');
   } catch (error) {
-    console.error('❌ Connection error:', error);
+    console.error(' Connection error:', error);
   }
 };
 
